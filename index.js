76b761eaa0f21c7e87140f275e96cac404b5c4bb
LiamD <contact@liamd.me>
2021-04-22 01:06:40 +0200

    Might have done something useless

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-window.navigator.__defineGetter__('userAgent', function () {
-	return 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Mobile/15E148 Safari/604.1';
+fetch(chrome.runtime.getURL('/main/index.html')).then(r => r.text()).then(html => {
+    const title = document.querySelector('title').innerText;
+
+    console.log(title);
+
+    document.querySelector('html').innerHTML = html
+    document.querySelectorAll('link')[0].setAttribute('href', chrome.runtime.getURL('/main/devices.min.css'))
+    document.querySelectorAll('link')[1].setAttribute('href', chrome.runtime.getURL('/main/main.css'))
+
+    
+    const iFrame = document.querySelector('iframe');
+
+    iFrame.setAttribute('src', window.location.href);
+    document.querySelector('.safari-top-bar .url-bar').innerHTML = `${isSecure() ? `<img src="${chrome.runtime.getURL('/main/assets/lock.svg')}">`: ''}${title}`;
+    if (isSecure()) document.querySelector('.safari-top-bar .url-bar').classList.add('secure');
+    else document.querySelector('.safari-top-bar .url-bar').classList.add('insecure');
+    document.querySelector('title').innerText = title;
 });
 
-fetch(chrome.runtime.getURL('/test.html')).then(r => r.text()).then(html => {
-    document.querySelector('html').innerHTML = html
-    document.querySelector('link').setAttribute('href', chrome.runtime.getURL('/device.min.css'))
-    
-    document.querySelector('iframe').setAttribute('src', window.location.href);
-});
+const isSecure = () => document.location.protocol === 'https:';
